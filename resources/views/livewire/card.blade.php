595864b4179f40be92471b22ddb343a102e3e32f
Jø GoldCity <jogoldcity@Js-MacBook-Pro.local>
2024-11-28 00:33:14 +0300

    responsive update

--- resources/views/livewire/card.blade.php
+++ resources/views/livewire/card.blade.php
@@ -1,11 +1,11 @@
-<div class="text-[#353844] flex overflow-hidden rounded-[8px]">
-    <div class="w-[184px] min-w-[184px]">
+<div class="text-[#353844] flex overflow-hidden rounded-[8px] max-[525px]:flex-col">
+    <div class="w-[184px] min-w-[184px] max-[525px]:w-full max-[525px]:bg-white max-[525px]:h-[180px]">
         @foreach ($images as $image)
-            <img src="{{ asset($image) }}" alt="" class=>
+            <img src="{{ asset($image) }}" alt="" class="object-cover min-w-full min-h-full h-full">
         @endforeach
     </div>
 
-    <div class="flex-1 bg-white pt-[40px] px-[34px]">
+    <div class="flex-1 bg-white pt-[40px] px-[34px] max-[525px]:p-[34px]">
         <h3 class="text-2xl font-[700]">{{ $title }}</h3>
 
         <ul class="text-[15px] font-[700] my-3">
--- resources/views/livewire/cards-page.blade.php
+++ resources/views/livewire/cards-page.blade.php
@@ -1,16 +1,17 @@
-<div class="flex items-center flex-col my-[108px]">
-    <h1 class="text-[52px] font-bold w-[730px] text-center text-[#1B264F] leading-[55px]">We make creative media that
+<div class="flex items-center flex-col my-[108px] max-[840px]:my-10 max-[840px]:px-10">
+    <h1 class="text-[52px] font-bold w-[730px] max-[840px]:w-auto text-center text-[#1B264F] leading-[55px]">We make
+        creative media that
         <span class="text-[#506BCA]">adds
             value</span>
     </h1>
 
-    <section class="my-[80px] grid grid-cols-3 gap-[30px] mx-[84px]">
-
+    <section
+        class="my-[80px] grid grid-cols-3 gap-[30px] mx-[84px] max-[840px]:mx-0 max-[1525px]:grid-cols-2 max-[1065px]:grid-cols-1">
         @foreach ($cards as $card)
             @livewire('card', ['images' => $card['images'], 'bullet_points' => $card['bullet_points'], 'title' => $card['title'], 'description' => $card['description']], key($card['title']))
         @endforeach
-
     </section>
+
     <button
         class="w-[315px] h-[60px] flex items-center justify-center bg-[#1B264F] rounded-[12px] text-[17px] text-white font-[700]">GET
         STARTED</button>
